622bdafec2d4ba42e915de98f10cf63d5cbe8e0d
Andrew Nunez <22259@columbushs.com>
2019-11-22 08:45:21 -0500

    almnost done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -2,19 +2,66 @@
 
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
 
-movies.sort( (a, b) => {
-    return a.year - b.year;
-  });
-  
-  movies.year.sort()
-  console.log()
+movies.sort((a, b) => {
+  return a.year - b.year
+}
+
+  movies.sort( (a, b) => {
+    if(a.title < b.title){
+    return -1
+   }
+    else if( b.title < a.title){
+      return 1;
+  }
+    else{
+      return 0;
+    }
+
+    return a.year - b.year
+
+
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
 
-function howManyMovies(anArray) {
-    anArray.filter(check(anArray));
+function howManyMovies([anArray]) {
+  if(genre == "drama" && director == "steven speilberg"){
+    return
+  }
+function howManyMovies(anArray){}
+anArray.filter(check(anArray));
+}
 
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
+
+function orderAlphabetically(array){
+
+let copy = [...array];
+
+  copy.sort( (a, b) => {
+    if(a.title < b.title){
+    return -1
+   }
+    else if( b.title < a.title){
+      return 1;
+  }
+    else{
+      return 0;
+    }
+    
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
 
